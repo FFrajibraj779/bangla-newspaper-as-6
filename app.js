c308a22f03e58ccc7926dbdad64a41015cdcd45e
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-09-03 14:16:55 +0600

    card design added

--- app.js
+++ app.js
@@ -68,67 +68,74 @@ const displayNewsDetails = async(newsId)=>{
 
    const newsDetails = document.getElementById('news-Details');
 
-   
+   newsDetails.innerHTML= '';
     
     
     data.forEach(news =>{
       
-      const{image_url, details, thumbnail_url, rating, title,author
-      } =news;
+      const{image_url, details, thumbnail_url, rating, title,author} =news;
  
    const{name, published_date, img}=author;
 
-      newsDetails.innerHTML =`
-      <div class="card mb-3" >
-      <div class="row g-0">
-        <div class="col-md-4">
-          <img src="${image_url}" class="img-fluid rounded-start w-100 h-100" alt="...">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h4 class="card-title">${title ? title : 'Not Found'}</h4>
-            <p class="card-text">${details ? details.slice(0, 200) : 'not found'}</p>
-             
-          
-        <div class="d-flex align-items-end justify-content-around">
-            
-        <div class="d-flex align-items-center">
-            <img src="${img}" class="img-fluid rounded-circle w-25 h-25" alt="...">
-
-            <div>
-                <p>${name}</p>
-                <p>${published_date}</p>
-            </div>
-
+    const div = document.createElement('div');
+    div.innerHTML=`
+    <div class="card mb-3" >
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="${image_url}" class="img-fluid rounded-start w-100 h-100" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h4 class="card-title">${title ? title : 'Not Found'}</h4>
+          <p class="card-text">${details ? details.slice(0, 200) : 'not found'}</p>
            
-        </div>
-
-        <div>
-            <p>${rating.number}</p>
-        </div>
-
-        <div>
-            <button class="btn btn-outline-dark">Show More</button>
-        </div>
-
- 
- </div>
+        
+      <div class="d-flex align-items-end justify-content-around">
           
-            
+      <div class="d-flex align-items-center">
+          <img src="${img}" class="img-fluid rounded-circle w-25 h-25" alt="...">
 
-           
+          <div>
+              <p>${name}</p>
+              <p>${published_date}</p>
           </div>
+
+         
+      </div>
+
+      <div>
+          <p>${rating.number}</p>
+      </div>
+
+      <div>
+          <button class="btn btn-outline-dark">Show More</button>
+      </div>
+
+
+</div>
+        
+          
+
+         
         </div>
       </div>
     </div>
-      `;
+  </div>
+    
+    `;
+
+      
+    newsDetails.appendChild(div);
+    
+     
+    
     })
     
 
      
        
 }    
-displayNewsDetails('');
+ 
 
  newsDetails();
 loadNews(); 
