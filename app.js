e6c8bab6d7a4b0ff53d38b42089edd5fdd168f2f
Md Rajikul Islame <mdrajikul779@gmail.com>
2022-09-03 19:05:24 +0600

    snipper added

--- app.js
+++ app.js
@@ -1,87 +1,91 @@
+
+const loadNews = async () => {
+   const url = `https://openapi.programming-hero.com/api/news/categories`;
+   const res = await fetch(url);
+   const data = await res.json();
+  return data.data;
  
- const loadNews = async()=>{
-    const url = `https://openapi.programming-hero.com/api/news/categories`;
-    const res = await fetch(url);
-     const data = await res.json();
-     return data.data;
- }
+}
 
 
 
-const showCategory= async() =>{
-   
-    const data = await loadNews();
-   
-    const{news_category} = data;
-   
+const showCategory = async ( ) => {
+
+   const data = await loadNews();
+
+   const { news_category } = data;
 
  const categorySection = document.getElementById('category-section');
+                  news_category.forEach(singleData => {
+      const { category_id, category_name } = singleData;
 
-       
-   news_category.forEach(singleData => {
-       const{category_id, category_name}=singleData;
-  
-     const div =document.createElement('div');
-    
+      const div = document.createElement('div');
+
       div.classList.add('mx-auto');
       div.classList.add('news-color');
 
       div.innerHTML = `
-     
       <ul>
  
-   <li onclick="AllnewsDetails('${category_id ? category_id : 'Empty'}')" >${category_name ?category_name : 'Not Found'}</li>
+   <li onclick="AllnewsDetails('${category_id ? category_id : 'Empty'}')" >${category_name ? category_name : 'Not Found'}</li>
        
   </ul>
    `;
       categorySection.appendChild(div);
-    
-   });
-}
-
-
-showCategory()
-
-
-
-
-const AllnewsDetails= async(category_id)=>{
-   const url=`https://openapi.programming-hero.com/api/news/category/${category_id ? category_id : 'Empty'}`;
-  
- fetch(url)
- .then(res =>res.json())
- .then(newsId=>displayNewsDetails(newsId))
-
  
-    
    
+   });
 
-       
+
+  
 }
 
-const displayNewsDetails = async(newsId)=>{
- 
-  
- const{data}= newsId;
+
+showCategory( '');
+
+const AllnewsDetails = async (category_id) => {
+   const url = `https://openapi.programming-hero.com/api/news/category/${category_id ? category_id : 'Empty'}`;
+
+       fetch(url)
+      .then(res => res.json())
+      .then(newsId => displayNewsDetails(newsId))
+
+   
+   //start loader
  
 
+}
+
+const displayNewsDetails = async (newsId) => {
+
+   const { data } = newsId;
+   
    const newsDetails = document.getElementById('news-Details');
+     
 
-   newsDetails.innerHTML= ' ';
-    
-    
-    data.forEach(news =>{
-       
-      const{image_url, details, rating, title, author, category_id} =news;
- 
-       const{name, published_date, img}=author;
+   newsDetails.innerHTML = ' ';
 
-       const div = document.createElement('div');
-       div.innerHTML=`
+   // const newsField = document.getElementById('news-field');
+   //      console.log(newsField);
+
+   // console.log(newsFieldValue.value);
+
+
+
+   data.forEach(news => {
+
+      toggleSnippers(true);
+
+      const { image_url, details, rating, title, author, category_id } = news;
+
+      const { name, published_date, img } = author;
+
+      const div = document.createElement('div');
+      div.innerHTML = `
        <div class="card mb-3 pb-2" >
           <div class="row g-0">
           <div class="col-md-4 ">
-         <img src="${image_url}" class="img-fluid rounded-start w-100 h-100" alt="...">
+         <img src="${image_url ? image_url : 'no image found'}" class="img-fluid rounded-start w-100 h-100" alt="...">
         
         </div>
          <div class="col-md-8">
@@ -122,56 +126,82 @@ const displayNewsDetails = async(newsId)=>{
 
     `;
 
-      
-    newsDetails.appendChild(div);
-    
-      
-    })
-    
-           
-}    
+
+      newsDetails.appendChild(div);
+
+
+   });
+
  
-const showModal =  ( category_id) =>{
-    
+   toggleSnippers(false);
+}
 
-   const url=`https://openapi.programming-hero.com/api/news/category/${category_id?category_id:'Empty'}`;
 
-    fetch(url)
-    .then(res=>res.json())
-    .then(dataNews =>displayShowModal(dataNews.data))
 
-           
-   
+
+
+const showModal = (category_id) => {
+
+      const url = `https://openapi.programming-hero.com/api/news/category/${category_id ? category_id : 'Empty'}`;
+
+      fetch(url)
+      .then(res => res.json())
+      .then(dataNews => displayShowModal(dataNews.data))
+
+  
+
 
 }
 
-const displayShowModal =(dataNews) =>{
-  
-   dataNews.forEach(newsModal =>{
-      console.log(newsModal);
-      const{image_url, details, rating, title, author} =newsModal;
+const displayShowModal = (dataNews) => {
 
-      const{name, published_date, img}=author;
+          console.log(dataNews);
+      dataNews.forEach(newsModal => {
+    
+      const { image_url, details, rating, title, author, total_view
+      } = newsModal;
 
-      const modalTitle=document.getElementById('exampleModalLabel');
-       modalTitle.innerText= title ? title : 'no data found';
-       const newsBody =document.getElementById('news-body');
-       newsBody.innerHTML=`
+      const { name, published_date, img } = author;
+
+      const modalTitle = document.getElementById('exampleModalLabel');
+      modalTitle.innerText = title ? title : 'no data found';
+      const newsBody = document.getElementById('news-body');
+      newsBody.innerHTML = `
 
        <div class="card"  >
-  <img src="${image_url}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h3 class="card-title"> ${name ? name : 'no data found'}</h3>
+    <img src="${image_url}" class="card-img-top" alt="...">
+     <div class="card-body">
+     <h3 class="card-title"> ${name ? name : 'no data found'}</h3>
            
-    <p class="card-text">${details.slice(0, 100) ? details.slice(0, 100) : 'no data found'}</p
+     <p class="card-text">${details.slice(0, 100) ? details.slice(0, 100) : 'no data found'}</p
+     <p>View: ${total_view ? total_view : 'data not found'}</p>
+     <p> Author: ${name ? name :'data not found'}</p>
+      
   
-  </div>
-</div>
+    </div>
+    </div>
        `;
- 
+
    })
- 
+
 }
 
- AllnewsDetails(' ');
+
+
+const toggleSnippers = isloading => {
+
+   const loaderSnippers = document.getElementById('loader');
+   
+
+   if (isloading) {
+      loaderSnippers.classList.remove('d-none');
+   }
+ else{
+   loaderSnippers.classList.add('d-none');
+ }
+}
+ 
+
+ 
+AllnewsDetails(' ');
 loadNews(' '); 
